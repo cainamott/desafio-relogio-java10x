4a57b7bbba9a4a738cf87a5f868258598e761dff
Cainã Motta <caina.tierling@gmail.com>
2026-02-25 21:33:39 -0300

    initial commit and h2 config

---
+++ desafiorelogio/src/main/java/github/cainamott/desafiorelogio/DesafiorelogioApplication.java
@@ -0,0 +1,13 @@
+package github.cainamott.desafiorelogio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiorelogioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiorelogioApplication.class, args);
+	}
+
+}
---
+++ desafiorelogio/src/test/java/github/cainamott/desafiorelogio/DesafiorelogioApplicationTests.java
@@ -0,0 +1,13 @@
+package github.cainamott.desafiorelogio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiorelogioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
